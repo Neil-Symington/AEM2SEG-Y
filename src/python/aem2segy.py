15f561361df12763bb7effce4ca063d89458f68f
u77932 <neil.symington@ga.gov.au>
2022-04-13 14:24:52 +1000

    add elevation top functionality

--- src/python/aem2segy.py
+++ src/python/aem2segy.py
@@ -3,26 +3,48 @@ import segyio
 import os
 
 # choose the vertical resampling interval
-yres = 4.0;
-max_depth = 500.;
-
+yres = 4.0
+max_depth = 400.
+scaling = 0.001 # convert from mS/m to S/m
 ##
-infile = r"C:\Users\u77932\Documents\NGS\data\AEM\EarheedyDesertStrip\GA_layer_earth_inversion\AusAEM_20_Earaheedy&DesertStrip_GA_layer_earth_inversion\Earaheedy& DesertStrip_AusAEM2020_GA_vsum_inversion.dat"
-outdir = r"C:\Users\u77932\Documents\NGS\data\AEM\EarheedyDesertStrip\GA_layer_earth_inversion\AusAEM_20_Earaheedy&DesertStrip_GA_layer_earth_inversion"
+infile = r"C:\Users\u77932\Documents\GAB\data\AEM\2017_survey\lci\Injune_WB_MGA55.dat"
+outdir = r"C:\Users\u77932\Documents\GAB\data\AEM\2017_survey\lci\segy"
 
-# define column numbers
-easting_col = 6
-northing_col = 7
-elevation_col = 8
-conductivity_cols = [22,51] #ranger for 30 layer model;
-thickness_cols = [52,81]
+# define column numbers with python indexing (i.e. columns 1 has index of 0)
+easting_col = 5
+northing_col = 6
+elevation_col =7
 line_col = 4
+conductivity_cols = [42,71] #range for 30 layer model;
+
+# depending on the inversion, the data may have thicknesses, layer_top_depths or layer_top_elevations
+# pick one and make the others = None
+thickness_cols = None
+layer_top_elevation_cols = [12,41]
+layer_top_depth_cols = None
 
 X = np.loadtxt(infile, usecols=[easting_col])
 Y = np.loadtxt(infile, usecols=[northing_col])
 Z = np.loadtxt(infile, usecols=[elevation_col])
-conductivity = np.loadtxt(infile, usecols=np.arange(conductivity_cols[0], conductivity_cols[1]+1))
-thicknesses = np.loadtxt(infile, usecols=np.arange(thickness_cols[0],thickness_cols[1]+1))
+conductivity = np.loadtxt(infile, usecols=np.arange(conductivity_cols[0], conductivity_cols[1]+1)) * scaling
+
+if thickness_cols is not None:
+    thicknesses = np.loadtxt(infile, usecols=np.arange(thickness_cols[0],thickness_cols[1]+1))
+    layer_top_elevations = np.zeros(shape = conductivity.shape, dtype = float)
+    layer_top_elevations[:,0] = Z
+    layer_top_elevations[:,1:] = layer_top_elevations[:,0] - np.cumsum(thicknesses[:,-1])
+
+elif layer_top_elevation_cols is not None:
+    layer_top_elevations = np.loadtxt(infile, usecols=np.arange(layer_top_elevation_cols[0],
+                                                                layer_top_elevation_cols[1]+1))
+elif layer_top_depth_cols is not None:
+    layer_top_depths = np.loadtxt(infile, usecols=np.arange(layer_top_depth_cols[0],
+                                                            layer_top_depth_cols[1] + 1))
+    layer_top_elevations = Z - layer_top_depths
+else:
+    print("please define column numbers for one of thickness, layer top elevation or layer top depths")
+
+
 lines = np.loadtxt(infile, usecols=[line_col]).astype(int)
 
 ##
@@ -31,7 +53,6 @@ def get_line_indices(line):
     inds = np.where(lines == line)[0]
     return inds
 
-
 def line2segy(line_number):
     # find our actual line indices
     line_inds = get_line_indices(line_number)
@@ -39,7 +60,7 @@ def line2segy(line_number):
     easting = X[line_inds]
     northing = Y[line_inds]
     elevation = Z[line_inds]
-    thickness = thicknesses[line_inds]
+    layer_top_elevation = layer_top_elevations[line_inds]
     sigma = conductivity[line_inds]
 
     # our first step is to define our datum as 20 m above the highest point in survey
@@ -60,11 +81,8 @@ def line2segy(line_number):
     # iterate
     for i in range(len(line_inds)):
 
-        layer_top_elev = elevation[i] * np.ones(shape = thickness.shape[1])
+        layer_top_elev = layer_top_elevation[i,:]
 
-        layer_top_elev[1:] = layer_top_elev[0] - np.cumsum(thickness[i,:-1])
-
-        #layer_top_elev = layer_top_elevation[i,:]
         sigma_trace = sigma[i]
         # iterate through our layers
         for j in range(len(layer_top_elev) - 1):
